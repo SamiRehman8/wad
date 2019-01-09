11a27cd76a4f4bb2bb3a84f0eb0a6bf3aa8f9f2b
SamiRehman8 <44521092+SamiRehman8@users.noreply.github.com>
2019-01-09 05:28:48 -0800

    Add files via upload
    quiz 5 updated done

---
+++ insert_category.php
@@ -0,0 +1,56 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Insert Product</title>
+    <title>Insert Category</title>
+    <meta name="viewport" content="width=device-width,initial-scale=1.0">
+    <link rel="stylesheet" href="../css/bootstrap.css">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Bangers|Old+Standard+TT">
+    <style>
+        * {
+            font-family: 'Old Standard TT', serif;
+        }
+    </style>
+    <?php
+    require_once "db_connection.php";
+    if(isset($_GET['insert_pro'])){
+    require "db_connection.php";
+    if(!isset($_POST['insert_pro'])){
+        //getting text data from the fields
+        $cat_title = $_POST['cat_title'];
+        //getting image from the field
+        $insert_cat = "insert into categories(cat_title) 
+                  VALUES ('$cat_title');";
+        $insert_pro = mysqli_query($con, $insert_cat);
+    }
+    ?>
+</head>
+<body>
+<div class="container">
+    <h1 class="text-center my-4"><i class="fas fa-plus fa-md"></i> <span class="d-none d-sm-inline"> Add New </span> Category </h1>
+    <form action="insert_product.php" method="post" enctype="multipart/form-data">
+        <form action="insert_cat.php" method="post">
+            <div class="row">
+                <div class="d-none d-sm-block col-sm-3 col-md-4 col-lg-2 col-xl-2 ">
+                    <label for="pro_cat" class="float-md-right"> <span class="d-sm-none d-md-inline"> Category </span> Title:</label>
+                </div>
+                <div class="col-sm-9 col-md-8 col-lg-4 col-xl-4">
+                    <div class="input-group">
+                        <div class="input-group-prepend">
+                            <div class="input-group-text"><i class="fas fa-file-signature"></i></div>
+                        </div>
+                        <input type="text" class="form-control" id="pro_cat" name="pro_cat" placeholder="Enter Category Title" >
+                        <input type="text" class="form-control" id="cat_title" name="cat_title" placeholder="Enter Category Title" >
+                    </div>
+                </div>
+                <div class="row my-3">
+                    <div class="d-none d-sm-block col-sm-3 col-md-4 col-lg-2 col-xl-2 mt-auto"></div>
+                    <div class="col-sm-9 col-md-8 col-lg-4 col-xl-4">
+                        <button type="submit" class="btn btn-primary btn-block" name="pro_insert"><i class="fas fa-plus"></i> Insert Now </button>
+                    </div>
+                </div>
+        </form>
+</div>
+</body>
+</html>
